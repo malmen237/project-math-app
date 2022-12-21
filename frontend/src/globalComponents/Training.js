34d43600af92c4bf6f5eea5497fd3c768a4b8947
malmen237 <linda.malm237@gmail.com>
2022-12-21 17:47:38 +0100

    Solved the dubbel-fetch and the button colors.

--- frontend/src/globalComponents/Training.js
+++ frontend/src/globalComponents/Training.js
@@ -11,61 +11,72 @@ const HeartBeatAnimation = keyframes`${pulse}`;
 
 const Training = () => {
   const [answer, setAnswer] = useState('');
-  const [nextQuestion, setNextQuestion] = useState(false);
-  const [goToNextQuestion, setGoToNextQuestion] = useState('false');
+  const [nextQuestion, setNextQuestion] = useState(true);
+  const [nextButton, setNextButton] = useState(false);
+  const [providedAnswer, setProvidedAnswer] = useState(false);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const operation = useSelector((state) => state.game.operation);
+  const problem = useSelector((state) => state.game.questions);
+  const problemNumber = useSelector((state) => state.game.currentProblemIndex);
+  const trainingOver = useSelector((state) => state.game.gameOver);
+  const isAnswerCorrect = useSelector((state) => state.game.isCorrect);
+
+  console.log('problem', problem)
+  // console.log('trainingOver', trainingOver)
+  // console.log('answer is:', isAnswerCorrect)
 
   const onFormSubmit = (event) => {
+    console.log('test1')
     event.preventDefault();
-    navigate('/summary')
+    navigate('/summary');
   }
 
   // Function that activates when user enters an answer,
   // also resets the goToNextQuestion-state hook
   const moveToNext = () => {
+    console.log('test2')
     dispatch(game.actions.submitAnswer(answer));
     setAnswer('');
+    setProvidedAnswer(true)
     dispatch(game.actions.goToNextQuestion());
-    setTimeout(() => { setNextQuestion(true) }, 3000);
-    setGoToNextQuestion(false);
+    setTimeout(() => { setNextQuestion(true) }, 2000);
+    setNextButton(false);
   }
 
   const handleUserAnswerInput = (event) => {
+    console.log('test3')
     setAnswer(event.target.value);
-    setGoToNextQuestion(true);
+    setNextButton(true);
   }
 
   // Get set of questions from database
   useEffect(() => {
-    setNextQuestion(false);
-    // To post type of math problems to be trained
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        operation
-      })
+    if (nextQuestion) {
+      setNextQuestion(false);
+      setProvidedAnswer(false);
+      console.log('test4')
+      // To post type of math problems to be trained
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          operation
+        })
+      }
+      fetch('http://localhost:8080/questions', options)
+        .then((res) => res.json())
+        .then((json) => {
+          console.log('test5')
+          dispatch(game.actions.submitQuestion(json.response));
+        })
     }
-    fetch('http://localhost:8080/questions', options)
-      .then((res) => res.json())
-      .then((json) => {
-        dispatch(game.actions.submitQuestion(json.response));
-      })
   }, [nextQuestion]);
 
-  const problem = useSelector((state) => state.game.questions);
-  console.log(problem)
-
-  const trainingOver = useSelector((state) => state.game.gameOver);
-  console.log('trainingOver', trainingOver)
-
-  const isAnswerCorrect = useSelector((state) => state.game.isCorrect);
-  console.log('answer is:', isAnswerCorrect)
   return (
     <>
       <h1>Question: {problem.question}</h1>
@@ -78,8 +89,9 @@ const Training = () => {
           onChange={(event) => handleUserAnswerInput(event)} />
         {!trainingOver && (
           <Button
-            className={goToNextQuestion ? (isAnswerCorrect ? 'correct' : 'wrong') : 'disabled'}
+            className={providedAnswer ? (isAnswerCorrect ? 'correct' : 'wrong') : 'default'}
             onClick={(event) => moveToNext(event)}
+            disabled={!nextButton}
             type="button">Next
           </Button>
         )}
@@ -87,6 +99,7 @@ const Training = () => {
           <button type="submit">Submit</button>
         )}
       </form>
+      <p>Question number {problemNumber + 1}</p>
     </>
   )
 }
@@ -97,7 +110,7 @@ const Button = styled.button`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: green;
+  background-color: lightblue;
   color: white;
   font-size: 1.5em;
   font-weight: bold;
@@ -110,17 +123,19 @@ const Button = styled.button`
   text-align: center;
 
   &.correct {
-    background-color: orange;
+    background-color: lightgreen;
+    color: white;
     animation: infinite 0.5s ${HeartBeatAnimation};
   }
 
   &.wrong {
     background-color: red;
+    color: white;
     animation: infinite 1s ${HeadShakeAnimation};
   }
 
   &:disabled {
-    color: rgb(0, 0, 0, 0.5);
+    color: rgb(0, 0, 0, 0.2);
   }
 `
 
