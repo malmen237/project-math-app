22330c0d4a79cc349fd5326d3b4feeff629d5c16
Tina Bruce <tinabruce42@hotmail.com>
2023-01-09 22:55:48 +0100

    Rearranged grid table displaying user training results, added table for challenge results and changed backend endpoint that gets users statistics.

--- backend/routes/userStatsRouter.js
+++ backend/routes/userStatsRouter.js
@@ -10,13 +10,29 @@ const router = express.Router()
 router.get("/:username",  async (req, res) => {
   const { username } = req.params;
   try {
-    const allStats = await UserStats.find({ username: username })
-    if (allStats.length > 0) {
+    // const allStats = await UserStats.find({ username: username }).sort({ score: 'desc' })
+    const trainStats = await UserStats.find({ username: username, quiztype: 'training' }).sort({ score: 'desc' })
+    const challengeStats = await UserStats.find({ username: username, quiztype: 'challenge' }).sort({ score: 'desc' })
+
+    // const topTrainStat = await UserStats.find({ username: username, quiztype: 'training' }).sort({ score: 'desc' }).limit(1)
+    // const topChallengeStat = await UserStats.find({ username: username, quiztype: 'challenge' }).sort({ score: 'desc' }).limit(1)
+    // const worstTrainStat = await UserStats.find({ username: username, quiztype: 'training' }).sort({ score: 'desc' }).limit(1)
+    // const worstChallengeStat = await UserStats.find({ username: username, quiztype: 'challenge' }).sort({ score: 'desc' }).limit(1)
+    const topTrainStat = trainStats[0]
+    const topChallengeStat = challengeStats[0]
+    const worstTrainStat = trainStats[trainStats.length - 1]
+    const worstChallengeStat = challengeStats[challengeStats.length - 1]
+
+    if (trainStats.length > 0 || challengeStats.length > 0){
       res.status(200).json({
         success: true,
-        response: allStats
-    })
-    } else {
+        response: { 
+          topTrainStat: topTrainStat,
+          topChallengeStat: topChallengeStat,
+          worstTrainStat: worstTrainStat,
+          worstChallengeStat: worstChallengeStat,
+        }
+    })} else {
       res.status(404).json({ 
         sucess: false,
         response: 'No stats found' 
--- frontend/src/components/userComponents/Statistics.js
+++ frontend/src/components/userComponents/Statistics.js
@@ -4,8 +4,15 @@ import { useSelector } from 'react-redux';
 import styled from 'styled-components/macro';
 
 const Statistics = () => {
-  const [stats, setStats] = useState([]);
-  console.log('stats', stats)
+  // const [trainStats, setTrainStats] = useState({});
+  // const [challengeStats, setChallengeStats] = useState({});
+  const [topTrainStat, setTopTrainStat] = useState({});
+  const [topChallengeStat, setTopChallengeStat] = useState({});
+  const [worstTrainStat, setWorstTrainStat] = useState({});
+  const [worstChallengeStat, setWorstChallengeStat] = useState({});
+  // const [challengeStats, setChallengeStats] = useState([]);
+  // console.log('trainstats', trainStats)
+  // console.log('Cstats', challengeStats)
 
   const username = useSelector((state) => state.user.username);
   console.log('username in statistics', username)
@@ -15,31 +22,68 @@ const Statistics = () => {
     fetch(`http://localhost:8080/userstats/${username}`)
       .then((res) => res.json())
       .then((json) => {
-        setStats(json.response)
+        setTopTrainStat(json.response.topTrainStat)
+        setTopChallengeStat(json.response.topChallengeStat)
+        setWorstTrainStat(json.response.worstTrainStat)
+        setWorstChallengeStat(json.response.worstChallengeStat)
+        // setChallengeStats(json.response.challengeStats)
+        // setTrainStats(json.response.trainStats[0])
+        // setChallengeStats(json.response.challengeStats[0])
         console.log('json.response', json.response);
       })
   }, []);
 
-  return (
-    <GridContainer>
-      <ColumnHeader>Quiz type</ColumnHeader>
-      <ColumnHeader>Category</ColumnHeader>
-      <ColumnHeader>Score</ColumnHeader>
-      <ColumnHeader>Points (Stars?)</ColumnHeader>
-      <ColumnHeader>Time</ColumnHeader>
-      <ColumnHeader>Opponent</ColumnHeader>
-      {stats.map((item) => (
-        <>
-          <GridItem>{item.quiztype}</GridItem>
-          <GridItem>{item.category}</GridItem>
-          <GridItem>{item.score} / 10</GridItem>
-          <GridItem>{item.points}</GridItem>
-          <GridItem>{item.time}</GridItem>
-          <GridItem>{item.opponent}</GridItem>
-        </>
-      ))}
-    </GridContainer>
+  // const topTrainStat = trainStats[0]
+  // const topChalStat = challengeStats[0]
+  // const worstTrainStat = trainStats[trainStats.length - 1]
+  // const worstChalStat = challengeStats[challengeStats.length - 1]
+  // console.log('topTrainStat typeof', typeof topTrainStat, topTrainStat)
+  // console.log('worstTrainStat typeof', typeof worstTrainStat, worstTrainStat)
 
+  return (
+    <>
+      <GridContainer>
+        <GridHeader>Your top results:</GridHeader>
+        <FillerHeader />
+        <TrainingHeader>Training</TrainingHeader>
+        <ChallengeHeader>Challenge</ChallengeHeader>
+
+        <RowHeader>Category</RowHeader>
+        <TrainGridItem>{topTrainStat.category}</TrainGridItem>
+        <ChallengeGridItem>{topChallengeStat.category}</ChallengeGridItem>
+        <RowHeader>Score</RowHeader>
+        <TrainGridItem>{topTrainStat.score}</TrainGridItem>
+        <ChallengeGridItem>{topChallengeStat.score}</ChallengeGridItem>
+        <RowHeader>Time</RowHeader>
+        <TrainGridItem>{topTrainStat.time}</TrainGridItem>
+        <ChallengeGridItem>{topChallengeStat.time}</ChallengeGridItem>
+        <RowHeader>Opponent</RowHeader>
+        <TrainGridItem>{topTrainStat.opponent}</TrainGridItem>
+        <ChallengeGridItem>{topChallengeStat.opponent}</ChallengeGridItem>
+      </GridContainer>
+
+      <GridContainer>
+        <GridHeader>Your least impressive results:</GridHeader>
+        <FillerHeader />
+        <TrainingHeader>Training</TrainingHeader>
+        <ChallengeHeader>Challenge</ChallengeHeader>
+
+        {/* Top results for training and challenge */}
+        <RowHeader>Category</RowHeader>
+        <TrainGridItem>{worstTrainStat.category}</TrainGridItem>
+        <ChallengeGridItem>{worstChallengeStat.category}</ChallengeGridItem>
+        <RowHeader>Score</RowHeader>
+        <TrainGridItem>{worstTrainStat.score}</TrainGridItem>
+        <ChallengeGridItem>{worstChallengeStat.score}</ChallengeGridItem>
+        <RowHeader>Time</RowHeader>
+        <TrainGridItem>{worstTrainStat.time}</TrainGridItem>
+        <ChallengeGridItem>{worstChallengeStat.time}</ChallengeGridItem>
+        <RowHeader>Opponent</RowHeader>
+        <TrainGridItem>{worstTrainStat.opponent}</TrainGridItem>
+        <ChallengeGridItem>{worstChallengeStat.opponent}</ChallengeGridItem>
+
+      </GridContainer>
+    </>
   )
 }
 
@@ -48,17 +92,71 @@ export default Statistics;
 const GridContainer = styled.section`
   display: grid;
   width: 90vw;
-  grid-template-columns: repeat(6, 1fr);
-  gap: 0.3rem;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 0.1rem;
+  margin: 2rem 0;
 `
-const ColumnHeader = styled.div`
+
+const GridHeader = styled.div`
+  grid-column: span 3;
+  background-color: lightcyan;
+  padding: 1rem;
+  font-size: larger;
+  border-radius: 0.3rem;
+`
+
+const FillerHeader = styled.div`
+  grid-column: 1/2;
+  gap: 0;
+  background-color: #FA5CB8;
+  border-radius: 0.3rem;
+`
+
+const TrainingHeader = styled.div`
+  grid-column: 2/3;
   background-color: #FA5CB8;
   font-weight: bold;
   padding: 1rem;
+  border-radius: 0.3rem;
 `
 
-const GridItem = styled.div`
+const ChallengeHeader = styled(TrainingHeader)`
+  grid-column: 3/4;
+  border-radius: 0.3rem;
+`
+
+const RowHeader = styled(TrainingHeader)`
+  grid-column: 1/2;
+  background-color: lightcoral;
+  border-radius: 0.3rem;
+`
+
+const TrainGridItem = styled.div`
+  grid-column: 2/3;
   background-color: #FACE75;
   padding: 0.5rem;
   font-size: larger;
+  border-radius: 0.3rem;
 `
+
+const ChallengeGridItem = styled(TrainGridItem)`
+  grid-column: 3/4;
+  border-radius: 0.3rem;
+`
+
+/* <ColumnHeader>Quiz type</ColumnHeader>
+      <ColumnHeader>Category</ColumnHeader>
+      <ColumnHeader>Score</ColumnHeader>
+      <ColumnHeader>Points (Stars?)</ColumnHeader>
+      <ColumnHeader>Time</ColumnHeader>
+      <ColumnHeader>Opponent</ColumnHeader>
+      {trainStats.map((item) => (
+        <>
+          <GridItem>{item.quiztype}</GridItem>
+          <GridItem>{item.category}</GridItem>
+          <GridItem>{item.score} / 10</GridItem>
+          <GridItem>{item.points}</GridItem>
+          <GridItem>{item.time}</GridItem>
+          <GridItem>{item.opponent}</GridItem>
+        </>
+      ))} */
