addfb4e8632c523c4f64e054e431a03ecc096d08
malmen237 <linda.malm237@gmail.com>
2023-01-10 10:20:59 +0100

    Removed duplicates from backend.

--- backend/routes/findUsernameRouter.js
+++ backend/routes/findUsernameRouter.js
@@ -7,20 +7,6 @@ router.get("/", async (req, res) => {
   const {username} = req.query;
   
   const usernameQuery = username ? username : /.*/gm;
-
-  // Get the count of all users
-  User.countDocuments().exec(function (err, count) {
-
-  // Get a random entry
-  let random = Math.floor(Math.random() * count)
-
-  // Again query all users but only fetch one offset by our random #
-  User.findOne().skip(random).exec(
-    function (err, result) {
-      // Tada! random user
-      console.log(result.username) 
-    })
-  })
   
   try {
     const response = await User.find({username: usernameQuery});
